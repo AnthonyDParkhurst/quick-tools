c8cbd89f5efc46a0d321ae67eafc9185f2c63d4d
Anthony Parkhurst <anthony.parkhurst@kofax.com>
2020-06-03 10:58:07 +0200

    Quick hex hack

---
+++ Hex2Bin/Hex2Bin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Hex2Bin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hex = File.ReadAllText(args[0]);
+
+            var bytes = Enumerable.Range(2, hex.Length - 2)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+
+            File.WriteAllBytes(args[1], bytes);
+        }
+    }
+}
