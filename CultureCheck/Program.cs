859351cb5cad63aedef3480e50377d0b73a7033c
Anthony Parkhurst <anthony.parkhurst@kofax.com>
2020-10-08 15:26:01 +0200

    just prints out CurrentCulture and CurrentUICulture

---
+++ CultureCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"CurrentUICulture: {System.Threading.Thread.CurrentThread.CurrentUICulture.Name}");
+            Console.WriteLine($"CurrentCulture: {System.Threading.Thread.CurrentThread.CurrentCulture.Name}");
+            Console.Write("Hit any key...");
+            Console.ReadKey();
+        }
+    }
+}
