6c3768bdb65b1f5c08d3e96c6c70c8c874050eec
Anthony Parkhurst <anthony.parkhurst@kofax.com>
2020-12-11 17:04:22 +0100

    Tool to delete faster

---
+++ JustDeleteIt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace JustDeleteIt
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var arg in args)
+                {
+                    if (File.Exists(arg))
+                    {
+                        Console.WriteLine($"Deleting file: {Path.GetFileName(arg)}");
+                        File.Delete(arg);
+                        continue;
+                    }
+
+                    Console.WriteLine($"Deleting directory tree: {Path.GetFileName(arg)}");
+                    Directory.Delete(arg, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nException: {ex.Message}");
+            }
+
+            Console.Write("\rEnter key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
