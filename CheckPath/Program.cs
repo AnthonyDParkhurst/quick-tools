028ec50d67f23021499e2469113d461e039016d4
Anthony Parkhurst <anthony.parkhurst@kofax.com>
2022-01-28 15:33:25 +0100

    small things

---
+++ CheckPath/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var pathVar = Environment.GetEnvironmentVariable("Path");
+
+if (pathVar == null)
+{
+    Environment.Exit(1);
+}
+
+var paths = pathVar.Split(";");
+
+var goodPaths = new List<string>();
+
+Console.WriteLine("Bad paths:");
+
+foreach (var path in paths)
+{
+    if (Directory.Exists(path))
+    {
+        goodPaths.Add(path);
+    }
+    else
+    {
+        Console.WriteLine($"bad: {path}");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Path={string.Join(";", goodPaths)}");
---
+++ FindFunnyChars/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var alltext = File.ReadAllText(args[0]);
+
+var index = 0;
+
+foreach (var c in alltext)
+{
+    if (c > 160)
+    {
+        Console.WriteLine($"Character at position: {index} is {(int)c}, or '{c}'");
+    }
+
+    index++;
+}
