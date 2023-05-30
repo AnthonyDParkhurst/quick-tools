cf7ed9da2039cdfeba33a6198aaa67c435644a53
Anthony Parkhurst <anthony.parkhurst@gmail.com>
2023-05-30 16:42:58 +0200

    Tool for stuffing guids in .wxs files

---
+++ GuidStuffer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GuidStuffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(args[0]);
+
+            var outLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                var newLine = line.Replace(@"Guid=""""", $@"Guid=""{Guid.NewGuid()}""");
+
+                outLines.Add(newLine);
+            }
+
+            File.WriteAllLines(args[0], outLines);
+        }
+    }
+}
