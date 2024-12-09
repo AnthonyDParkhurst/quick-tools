5e866b5f061a8240184d18d7e45f4aa0b8766a89
Anthony Parkhurst <30617611+AnthonyDParkhurst@users.noreply.github.com>
2024-12-09 12:50:33 +0100

    Quick tool to split log files

---
+++ SplitFile/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace SplitFile
+{
+    internal class Program
+    {
+        private const int MaxLines = 500000;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: SplitFile <filename");
+                Environment.Exit(0);
+            }
+
+            foreach (var filename in args)
+            {
+                Split(filename);
+            }
+        }
+
+        private static void Split(string filename)
+        {
+            var dirname = Path.GetDirectoryName(filename);
+            var baseName = Path.GetFileNameWithoutExtension(filename);
+            var extension = Path.GetExtension(filename);
+
+            var index = 0;
+
+            using var input = File.OpenText(filename);
+
+            while (input.EndOfStream == false)
+            {
+                var outputFilename = Path.Combine(dirname, baseName + $"_{index}" + extension);
+
+                Console.WriteLine($"{outputFilename}...");
+
+                index++;
+
+                var lines = new List<string>(MaxLines);
+
+                for (var lineNumber = 0; lineNumber < MaxLines; lineNumber++)
+                {
+                    if (input.EndOfStream)
+                    {
+                        break;
+                    }
+
+                    lines.Add(input.ReadLine());
+                }
+
+                File.WriteAllLines(outputFilename, lines);
+
+                lines.Clear();
+            }
+        }
+    }
+}
