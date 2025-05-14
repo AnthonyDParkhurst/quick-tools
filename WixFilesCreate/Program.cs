82445c5238d8d988ca8f7b592755a1f17769d2eb
Anthony Parkhurst <30617611+AnthonyDParkhurst@users.noreply.github.com>
2025-05-14 10:13:38 +0200

    Update .net version

--- WixFilesCreate/Program.cs
+++ WixFilesCreate/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
